337ba2f9b18a8df450478d6b3794510bda2df30a
Damien Le Berrigaud <damien@itsdamo.com>
2017-01-03 14:42:45 -0700

    Replace AddUser Struts Action with a Spring controller action.

--- src/main/java/org/superbiz/struts/UsersController.java
+++ src/main/java/org/superbiz/struts/UsersController.java
@@ -1,13 +1,34 @@
 package org.superbiz.struts;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UsersController {
 
-    @GetMapping("/addUserForm")
+    private final UserService userService;
+
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/addUser")
     public String addUserForm() {
         return "addUserForm";
+    }
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user, Model model) {
+        try {
+            userService.add(user);
+        } catch (Exception | Error e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "addUserForm";
+        }
+
+        return "addedUser";
     }
 }
