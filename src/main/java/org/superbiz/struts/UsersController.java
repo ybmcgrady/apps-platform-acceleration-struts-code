d89a961ab23a4d573f37eed5abe3df94483453ee
bo yu <bo.yu@cognizant.com>
2018-03-05 11:18:13 -0700

    my-struts-to-spring solutino

---
+++ src/main/java/org/superbiz/struts/UsersController.java
@@ -0,0 +1,61 @@
+package org.superbiz.struts;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UsersController {
+
+    private final UserRepository userRepository;
+
+    public UsersController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/addUser")
+    public String addUserForm() {
+        return "addUserForm";
+    }
+
+
+    @PostMapping("/addUser")
+    public String addUser(@ModelAttribute User user, Model model) {
+        try {
+            userRepository.save(user);
+        } catch (Exception | Error e) {
+            model.addAttribute("errorMessage", e.getMessage());
+            return "addUserForm";
+        }
+
+        return "addedUser";
+    }
+
+
+    @GetMapping("/findUser")
+    public String findUserForm() {
+        return "findUserForm";
+    }
+
+    @PostMapping("/findUser")
+    public String findUser(@RequestParam long id, Model model) {
+        User user = userRepository.findOne(id);
+
+        if (user == null) {
+            model.addAttribute("errorMessage", "User not found");
+            return "findUserForm";
+        }
+
+        model.addAttribute("user", user);
+        return "displayUser";
+    }
+
+    @GetMapping("/list")
+    public String listUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "displayUsers";
+    }
+}
